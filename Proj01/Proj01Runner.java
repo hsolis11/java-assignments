482166c2784088cba7c53d882f16bdd54658863a
Hector Solis <hsolis11@outlook.com>
2021-01-19 23:00:22 -0600

    Added notes

--- Proj01/Proj01Runner.java
+++ Proj01/Proj01Runner.java
@@ -8,24 +8,25 @@ public class Proj01Runner {
 
     public Proj01Runner(int[] vals){
         this.vals = vals;
+        
         for(int i = 0; i < vals.length; i++){
             ref.add(vals[i]);
-        }
+        }//end for loop
 
         System.out.println("I certify that this program is my own work\n" +
                             "and is not the work of others. I agree not\n" +
                             "to share my solution with others.\n" +
                             "Hector Solis\n");
-    }
+    }//end Constructor
 
     public Collection runA(){
         return ref;
-    }
+    }//end runA method
 
     public Collection runB(){
         return ref;
-    }
+    }//end runB method
     
 
     
-}
+}//end Proj01Runner class
