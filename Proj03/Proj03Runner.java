a3ff9437f87e60e0507d69127152a2dbc40b993b
Hector Solis <hsolis11@outlook.com>
2021-01-19 22:00:05 -0600

    Adding Proj01 and Proj03

---
+++ Proj03/Proj03Runner.java
@@ -0,0 +1,23 @@
+public class Proj03Runner {
+
+    private String listName;
+
+    public Proj03Runner() {
+        System.out.println("I certify that this program is my own work" +
+                            "and is not the work of others. I agree not " +
+                            "to share my solution with others. " +
+                            "Hector Solis");
+    }//end Proj03Runner constructor
+ 
+    public String Proj03Runner(String value) {
+        
+        this.listName = value;
+        return this.listName;
+    }//end Proj03Runner overloaded constructor
+
+    public static void main(String[] args){
+        //
+
+    }//end main method
+
+}
