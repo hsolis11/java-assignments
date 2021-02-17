c21be49e55e620d97c8da0d5ea0ec19d2f76e3ce
hsolis <hsolis11@outlook.com>
2021-02-17 16:46:28 -0600

    Made comparator work

--- Proj03/Proj03Runner.java
+++ Proj03/Proj03Runner.java
@@ -1,51 +1,41 @@
 import java.io.Serializable;
 import java.util.*;
 
-public class Proj03Runner {
+public class Proj03Runner implements Comparator, Serializable{
 
-    int data;
     String str_data;
 
     Proj03Runner() {
-        System.out.println("I certify that this program is my own work" +
-                            "and is not the work of others. I agree not " +
-                            "to share my solution with others. " +
-                            "Hector Solis");
+        System.out.println("I certify that this program is my own work\n" +
+                            "and is not the work of others. I agree not\n" +
+                            "to share my solution with others.\n" +
+                            "Hector Solis\n");
+        str_data = "dummy";
     }//end no arg constructor
-
-    Proj03Runner(int data) {
-        this.data = data;
-    }
 
     Proj03Runner(String str_data){
         this.str_data = str_data;
     }
 
-    public String toString() {
-        return "" + data;
-    }
-
-}
-
-class TheComparator implements Comparator, Serializable{
-
     public int compare(Object o1, Object o2){
-        if(!(o1 instanceof Proj03Runner)){
+        if(!(o1 instanceof String)){
             throw new ClassCastException();
         }
-        if(!(o2 instanceof Proj03Runner)){
+        if(!(o2 instanceof String)){
             throw new ClassCastException();
         }
-        
-        int result = ((String)o1).compareTo(((String)o2));
-        return result;
+
+        int result = ((String)o1).toUpperCase().compareTo(((String)o2).toUpperCase());
+        return result*(-1);
     }
 
     public boolean equals(Object o){
-        if(!(o instanceof TheComparator)){
+        if(!(o instanceof Proj03Runner)){
             return false;
         } else {
             return true;
         }
     }
+
 }
+
