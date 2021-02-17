ecab4c546d04ac784b980f701160a7a302f9919e
hsolis <hsolis11@outlook.com>
2021-02-17 10:20:25 -0600

    Added comparable

--- Proj03/Proj03Runner.java
+++ Proj03/Proj03Runner.java
@@ -1,23 +1,51 @@
+import java.io.Serializable;
+import java.util.*;
+
 public class Proj03Runner {
 
-    private String listName;
+    int data;
+    String str_data;
 
-    public Proj03Runner() {
+    Proj03Runner() {
         System.out.println("I certify that this program is my own work" +
                             "and is not the work of others. I agree not " +
                             "to share my solution with others. " +
                             "Hector Solis");
-    }//end Proj03Runner constructor
- 
-    public String Proj03Runner(String value) {
+    }//end no arg constructor
+
+    Proj03Runner(int data) {
+        this.data = data;
+    }
+
+    Proj03Runner(String str_data){
+        this.str_data = str_data;
+    }
+
+    public String toString() {
+        return "" + data;
+    }
+
+}
+
+class TheComparator implements Comparator, Serializable{
+
+    public int compare(Object o1, Object o2){
+        if(!(o1 instanceof Proj03Runner)){
+            throw new ClassCastException();
+        }
+        if(!(o2 instanceof Proj03Runner)){
+            throw new ClassCastException();
+        }
         
-        this.listName = value;
-        return this.listName;
-    }//end Proj03Runner overloaded constructor
+        int result = ((String)o1).compareTo(((String)o2));
+        return result;
+    }
 
-    public static void main(String[] args){
-        //
-
-    }//end main method
-
+    public boolean equals(Object o){
+        if(!(o instanceof TheComparator)){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
