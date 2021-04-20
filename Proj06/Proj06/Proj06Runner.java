9bcdcfd98cfdf87b9e2546cdbe4867e76d3a64c8
Hector Solis <hsolis11@outlook.com>
2021-04-19 22:28:16 -0500

    Finished assignment

--- Proj06/Proj06/Proj06Runner.java
+++ Proj06/Proj06/Proj06Runner.java
@@ -8,93 +8,61 @@ public class Proj06Runner {
                          "to share my solution with others.\n" +
                          "Hector Solis\n");
 
-    GUI gui = new GUI();//instantiate a GUI
-  }//end main
-}//end class Event10
-//=======================================================//
+    GUI gui = new GUI();
+  }//end Proj06Runner constructor
+}//end class Proj06Runner
 
-//Subclass Frame in order to override the paint method.
+
 class MyFrame extends Frame{
   int xCoor;
   int yCoor;
 
-  MyFrame(String name){//constructor
+  MyFrame(String name){
     setTitle("Hector Solis");
     setSize(300,100);
-    //Name used to distinguish between the two objects
+    
     setName(name);
-  }//end constructor
+  }//end MyFrame constructor
   
   public void paint(Graphics g){
-    //display coordinate information on the visual object
     g.setColor(Color.RED);
     g.drawString("x = " + xCoor + ", y = " + yCoor, xCoor, yCoor);
     
   }//end paint()
 }//end class MyFrame
-//=======================================================//
 
-//The following class is used to instantiate a graphical
-// user interface object.
+
 class GUI {
-  public GUI(){//constructor
-    //Create two visual objects of type MyFrame and make 
-    // them visible. Name them Frame1 and Frame2.
+  public GUI(){
     MyFrame myFrame1 = new MyFrame("Frame1");
     myFrame1.setVisible(true); 
     
-    //Instantiate and register Listener object which will
-    // terminate the program when the user closes either 
-    // window.
     WProc1 winProcCmd1 = new WProc1();
     myFrame1.addWindowListener(winProcCmd1);
     
-    //Instantiate and register Listener object which will
-    // process mouse events on either MyFrame object.
     MouseProc mouseProcCmd = new MouseProc(myFrame1);
     myFrame1.addMouseListener(mouseProcCmd);
-  }//end constructor
-}//end class GUI definition
-//=======================================================//
+  }//end GUI constructor
+}//end class GUI
 
-//This listener class monitors for mouse presses and 
-// displays the coordinates of the mouse pointer when the 
-// mouse is pressed.  The listener object distinguishes 
-// between two different visual objects on the basis of 
-// their component names and displays the coordinate 
-// information on the visual object which generated the 
-// mouse event.
 
 class MouseProc extends MouseAdapter{
-  //save references to the objects here
   MyFrame refToFrame1; 
   
-  MouseProc(MyFrame inFrame1){//construct
-    refToFrame1 = inFrame1;//save references to the frames
+  MouseProc(MyFrame inFrame1){
+    refToFrame1 = inFrame1;
   }//end constructor
 
-  //Override the mousePressed() method to respond whenever
-  // the mouse is pressed on one of the frame objects.  
-  // Distinguish between the two frame objects using the 
-  // component name and display the coordinates of the 
-  // mouse on the correct object.
   public void mousePressed(MouseEvent e){
     if( e.getComponent().getName().compareTo("Frame1") == 0 ){
-      //Get X and Y coordinates of mouse pointer
-      // and store in the Frame object
       refToFrame1.xCoor = e.getX();
       refToFrame1.yCoor = e.getY();
-      //display coordinate information
+      
       refToFrame1.repaint();
     }
     }//end if-else
   }//end mousePressed()
-//end class MouseProc
-//=======================================================//
 
-//The following listener is used to terminate the program
-// when the user closes either frame object. Note that 
-// class extends the adapter class
 class WProc1 extends WindowAdapter{
   public void windowClosing(WindowEvent e){
     System.exit(0);
